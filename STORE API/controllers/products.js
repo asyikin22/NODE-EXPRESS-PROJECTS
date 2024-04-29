66a45445556c66be65a34c8f0bb14e2c156a0c86
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-04-29 20:07:21 +0800

    database queries

--- STORE API/controllers/products.js
+++ STORE API/controllers/products.js
@@ -1,14 +1,28 @@
+const { query } = require('express')
 const Product = require('../models/product-models')
 
 const getAllProductsStatic = async (req, res) => {
     const products = await Product.find({
-        name: 'vase table'
+        name: {$regex: search, $options: 'i'},
     })
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query)
+    const { featured, company, name } = req.query
+    const queryObject = {}
+
+    if ( featured ) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if(company) {
+        queryObject.company = company 
+    }
+    if(name) {
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -16,4 +30,5 @@ module.exports = {
     getAllProducts,
     getAllProductsStatic
 }
+
 
